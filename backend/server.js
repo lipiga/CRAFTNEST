dbdb66a075750ce8cdb7c9469cf2e7c4a90b218c
lipiga <lipiga.a2004@gmail.com>
2025-11-16 16:10:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(__dirname,'public')))
 
 
 
-mongoose.connect("mongodb+srv://LipigaAlagarsamy:agipil175@cluster0.00nca4d.mongodb.net/frostbyte").then(console.log("Database Connected"))
+mongoose.connect("mongodb+srv://DBUSERNAME:DBPASSWORD5@cluster0.00nca4d.mongodb.net/frostbyte").then(console.log("Database Connected"))
 
 
 
